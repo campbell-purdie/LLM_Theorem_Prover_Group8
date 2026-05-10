b1283996f9e6499631bdb299909759021b35b486
Campbell Purdie <110272102+campbell-purdie@users.noreply.github.com>
2026-05-10 14:26:07 +1000

    Delete llm-isabelle directory

--- llm-isabelle/tmp/ContextDemo.thy
+++
@@ -1,9 +0,0 @@
-theory ContextDemo imports Main begin
-
-definition map_compose where "map (f ∘ g) xs = map f (map g xs)"
-
-lemma append_assoc: "(xs @ ys) @ zs = xs @ (ys @ zs)"
-lemma map_append: "map f (xs @ ys) = map f xs @ map f ys"
-lemma rev_rev: "rev (rev xs) = xs"
-
-end
